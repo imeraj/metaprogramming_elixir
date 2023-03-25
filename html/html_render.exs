ca4f6db3d92a089f5d245f45fb75f393bbd4a0e9
Meraj <meraj.enigma@gmail.com>
2023-03-25 15:52:54 -0400

    supports all HTML tags

--- html/html_render.exs
+++ html/html_render.exs
@@ -3,16 +3,28 @@ defmodule Template do
 
   def render do
     markup do
-      tag :table do
-        tag :tr do
+      table do
+        tr do
           for i <- 0..5 do
-            tag(:td, do: text("Cell #{i}"))
+            td(do: text("Cell #{i}"))
           end
         end
       end
 
-      tag :div do
+      div do
         text("Some nested content")
+      end
+
+      div id: "main" do
+        h1 class: "title" do
+          text("Welcome!")
+        end
+      end
+
+      div class: "row" do
+        div do
+          p(do: text("Hello!"))
+        end
       end
     end
   end
